6167d55fa302f9005058b0b22b5e41c4418d5605
1363534009@qq.com <1363534009@qq.com>
2020-06-01 19:01:30 +0800

    更新启动类注解

--- src/main/java/com/yuyue/pagehelper/SpringbootPagehelperApplication.java
+++ src/main/java/com/yuyue/pagehelper/SpringbootPagehelperApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 这是一个启动类
+ */
 @SpringBootApplication
 @MapperScan("com.yuyue.pagehelper.mapper")
 public class SpringbootPagehelperApplication {
