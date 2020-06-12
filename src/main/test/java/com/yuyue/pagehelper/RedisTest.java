3e8b1665d216d91066e7a7e8e85044eef2db04f0
yy1363534009 <yy1363534009@163.com>
2020-06-12 14:36:20 +0800

    整合redis

--- src/main/test/java/com/yuyue/pagehelper/RedisTest.java
+++ src/main/test/java/com/yuyue/pagehelper/RedisTest.java
@@ -6,10 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Title: RedisTest * @Description:redis test
@@ -25,13 +22,11 @@ public class RedisTest {
     private RedisTemplate redisTemplate;
 
     @Test
-    @Transactional
     public void test01() {
 
-        // redisTemplate.opsForValue().set("birthday", "1996年11月11日");
+        redisTemplate.opsForValue().set("sex", "男");
         Assert.assertEquals("比较", "男", redisTemplate.opsForValue().get("sex"));
-        System.out.println(redisTemplate.opsForValue().get("address"));
-        System.out.println(redisTemplate.opsForValue().get("birthday"));
+        System.out.println(redisTemplate.opsForValue().get("sex"));
 
     }
 
